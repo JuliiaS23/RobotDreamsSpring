d7742e41941eca3112c47e940361763ebf7306f8
JuliiaS2310 <julia.satsyk@gmail.com>
2023-06-04 11:56:47 +0300

    Initial commit

---
+++ src/main/java/com/ua/robot/robotdreamsspring/RobotDreamsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ua.robot.robotdreamsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotDreamsSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RobotDreamsSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ua/robot/robotdreamsspring/RobotDreamsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ua.robot.robotdreamsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotDreamsSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
